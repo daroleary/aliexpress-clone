3ab68ea5b456af22498aa048c21eb19983092a25
Darren O'Leary <darren@costcertified.com>
2024-02-11 09:56:34 -0700

    Initial project setup with nuxt

---
+++ layouts/MainLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="MainLayout" class="fixed z-50 w-full">
+    <div id="TopMenu" class="w-full border-b bg-[#F2F2F2] md:block">
+      <ul
+        class="flex h-10 max-w-[1200px] items-center justify-end bg-[#FAFAFA] px-2 text-xs font-light text-[#333333]"
+      >
+        <li class="cursor-pointer border-r border-r-gray-400 px-3 hover:text-[#FF4636]">
+          Sell on AliExpress
+        </li>
+        <li class="cursor-pointer border-r border-r-gray-400 px-3 hover:text-[#FF4636]">
+          Cookie Preferences
+        </li>
+        <li class="cursor-pointer border-r border-r-gray-400 px-3 hover:text-[#FF4636]">Help</li>
+        <li class="cursor-pointer border-r border-r-gray-400 px-3 hover:text-[#FF4636]">
+          Buyer Protection
+        </li>
+        <li class="cursor-pointer px-3 hover:text-[#FF4636]">Sell on AliExpress</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div id="IndexPage" class="mx-auto mt-4 max-w-[1200px] px-2">Index Page</div>
+    <MainLayout />
+  </div>
+</template>
+
+<script setup>
+import MainLayout from '~/layouts/MainLayout.vue'
+</script>
