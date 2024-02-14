91cac557dbdb9b4a23fdecdca7f45c495ba58290
Darren O'Leary <darren@costcertified.com>
2024-02-13 22:40:45 -0700

    - Adding BaseProductComponent to display product details on main layout

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -1,132 +1,142 @@
 <template>
-  <div id="MainLayout" class="fixed z-50 w-full">
-    <div id="TopMenu" class="w-full border-b bg-[#FAFAFA] md:block hidden">
-      <ul
-        class="flex h-10 max-w-[1200px] items-center justify-end bg-[#FAFAFA] px-2 text-xs font-light text-[#333333]"
-      >
-        <li class="cursor-pointer border-r border-r-gray-400 px-3 hover:text-[#FF4636]">
-          Sell on AliExpress
-        </li>
-        <li class="cursor-pointer border-r border-r-gray-400 px-3 hover:text-[#FF4636]">
-          Cookie Preferences
-        </li>
-        <li class="cursor-pointer border-r border-r-gray-400 px-3 hover:text-[#FF4636]">Help</li>
-        <li class="cursor-pointer border-r border-r-gray-400 px-3 hover:text-[#FF4636]">
-          Buyer Protection
-        </li>
-        <li class="px-3 hover:text-[#FF4646] cursor-pointer">
-          <Icon name="ic:sharp-install-mobile" size="17" />
-          App
-        </li>
-        <li
-          class="relative flex items-center px-2.5 hover:text-[#FF4646] h-full cursor-pointer"
-          :class="
-            isAccountMenu
-              ? 'bg-white border z-40 shadow-[0_15px_100px_40px_rgba(0,0,0,0.3)]'
-              : 'border border-[#FAFAFA]'
-          "
-          @mouseenter="isAccountMenu = true"
-          @mouseleave="isAccountMenu = false"
+  <div>
+    <div id="MainLayout" class="w-full fixed z-50">
+      <div id="TopMenu" class="w-full bg-[#FAFAFA] border-b md:block hidden">
+        <ul
+          class="flex items-center justify-end text-xs text-[#333333] font-light px-2 h-10 bg-[#FAFAFA] max-w-[1200px]"
         >
-          <Icon name="ph:user-thin" size="17" />
-          Account
-          <Icon name="mdi:chevron-down" size="15" class="ml-5" />
-          <div
-            v-if="isAccountMenu"
-            id="AccountMenu"
-            class="absolute bg-white w-[220px] text-[#333333] z-40 top-[38px] -left-[100px] border-x border-b"
+          <li class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer">
+            Sell on AliExpress
+          </li>
+          <li class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer">
+            Cookie Preferences
+          </li>
+          <li class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer">Help</li>
+          <li class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer">
+            Buyer Protection
+          </li>
+          <li class="px-3 hover:text-[#FF4646] cursor-pointer">
+            <Icon name="ic:sharp-install-mobile" size="17" />
+            App
+          </li>
+          <li
+            class="relative flex items-center px-2.5 hover:text-[#FF4646] h-full cursor-pointer"
+            :class="
+              isAccountMenu
+                ? 'bg-white border z-40 shadow-[0_15px_100px_40px_rgba(0,0,0,0.3)]'
+                : 'border border-[#FAFAFA]'
+            "
+            @mouseenter="isAccountMenu = true"
+            @mouseleave="isAccountMenu = false"
           >
-            <div v-if="true">
-              <div class="text-semibold text-[15px] my-4 px-3">Welcome to AliExpress!</div>
-              <div class="flex items-center gap-1 px-3 mb-3">
-                <NuxtLink
-                  to="/auth"
-                  class="bg-[#FF4646] text-center w-full text-[16px] rounded-sm text-white font-semibold"
-                >
-                  Login / Register
-                </NuxtLink>
-              </div>
-            </div>
-            <div class="border-b" />
-            <ul class="bg-white">
-              <li
-                class="text-[13px]py-2 px-4 w-full hover:bg-gray-200"
-                @click="navigateTo('/orders')"
-              >
-                My Orders
-              </li>
-              <li v-if="true" class="text-[13px] py-2 px-4 w-full hover:bg-gray-200">Sign Out</li>
-            </ul>
-          </div>
-        </li>
-      </ul>
-    </div>
-    <div id="MainHeader" class="flex items-center w-full bg-white">
-      <div
-        class="flex lg:justify-start justify-between gap-10 max-w-[1150px] w-full px-3 py-5 mx-auto"
-      >
-        <NuxtLink to="/" class="flex items-center">
-          <img src="/AliExpress-logo.png" alt="AliExpress" width="170" />
-        </NuxtLink>
+            <Icon name="ph:user-thin" size="17" />
+            Account
+            <Icon name="mdi:chevron-down" size="15" class="ml-5" />
 
-        <div class="max-w-[700px] w-full md:block hidden">
-          <div class="relative">
-            <div class="flex items-center border-2 border-[#FF4646] rounded-md w-full">
-              <input
-                v-model="searchItem"
-                class="w-full placeholder-gray-400 text-sm pl-3 focus:outline-none"
-                type="text"
-                placeholder="kitchen accessories"
-              />
-              <Icon v-if="isSearching" name="eos-icons:loading" size="25" class="mr-2" />
-              <button class="flex items-center h-[100%] p-1.5 px-2 bg-[#FF4646]">
-                <Icon name="ph:magnifying-glass" size="20" color="#ffffff" />
-              </button>
-            </div>
             <div
-              class="absolute bg-white max-w-[700px] h-auto w-full cursor-pointer hover:bg-gray-100"
+              v-if="isAccountMenu"
+              id="AccountMenu"
+              class="absolute bg-white w-[220px] text-[#333333] z-40 top-[38px] -left-[100px] border-x border-b"
             >
-              <div v-if="false" class="p-1">
-                <NuxtLink
-                  to="`/item/1`"
-                  class="flex items-center justify-between w-full cursor-pointer hover:bg-gray-100"
+              <div v-if="!user">
+                <div class="text-semibold text-[15px] my-4 px-3">Welcome to AliExpress!</div>
+                <div class="flex items-center gap-1 px-3 mb-3">
+                  <NuxtLink
+                    to="/auth"
+                    class="bg-[#FF4646] text-center w-full text-[16px] rounded-sm text-white font-semibold p-2"
+                  >
+                    Login / Register
+                  </NuxtLink>
+                </div>
+              </div>
+              <div class="border-b" />
+              <ul class="bg-white">
+                <li
+                  class="text-[13px] py-2 px-4 w-full hover:bg-gray-200"
+                  @click="navigateTo('/orders')"
                 >
-                  <div class="flex items-center">
-                    <img class="rounded-md" width="40" src="https://picsum.photos/id/82/300/300" />
-                    <div class="truncate ml-2">TESTING</div>
-                  </div>
-                  <div class="truncate">$ 98.99</div>
-                </NuxtLink>
+                  My Orders
+                </li>
+                <li
+                  v-if="user"
+                  class="text-[13px] py-2 px-4 w-full hover:bg-gray-200"
+                  @click="client.auth.signOut()"
+                >
+                  Sign out
+                </li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div id="MainHeader" class="flex items-center w-full bg-white">
+        <div
+          class="flex lg:justify-start justify-between gap-10 max-w-[1150px] w-full px-3 py-5 mx-auto"
+        >
+          <NuxtLink to="/" class="min-w-[170px]">
+            <img width="170" src="/AliExpress-logo.png" />
+          </NuxtLink>
+
+          <div class="max-w-[700px] w-full md:block hidden">
+            <div class="relative">
+              <div class="flex items-center border-2 border-[#FF4646] rounded-md w-full">
+                <input
+                  v-model="searchItem"
+                  class="w-full placeholder-gray-400 text-sm pl-3 focus:outline-none"
+                  placeholder="kitchen accessories"
+                  type="text"
+                />
+                <Icon v-if="isSearching" name="eos-icons:loading" size="25" class="mr-2" />
+                <button class="flex items-center h-[100%] p-1.5 px-2 bg-[#FF4646]">
+                  <Icon name="ph:magnifying-glass" size="20" color="#ffffff" />
+                </button>
+              </div>
+
+              <div v-if="items && items.data" class="absolute bg-white max-w-[700px] h-auto w-full">
+                <div class="p-1">
+                  <NuxtLink
+                    :to="`/item/${item.id}`"
+                    class="flex items-center justify-between w-full cursor-pointer hover:bg-gray-100"
+                  >
+                    <div class="flex items-center">
+                      <img class="rounded-md" width="40" :src="item.url" />
+                      <div class="truncate ml-2">{{ item.title }}</div>
+                    </div>
+                    <div class="truncate">${{ item.price / 100 }}</div>
+                  </NuxtLink>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <NuxtLink to="/shoppingcart" class="flex items-center">
-          <button
-            class="relative md:block hidden"
-            @mouseenter="isCartHover = true"
-            @mouseleave="isCartHover = false"
-          >
-            <span
-              class="absolute flex items-center justify-center -right-[3px] top-0 bg-[#FF4646] h-[17px] min-w-[17px] text-xs text-white px-0.5 rounded-full"
+
+          <NuxtLink to="/shoppingcart" class="flex items-center">
+            <button
+              class="relative md:block hidden"
+              @mouseenter="isCartHover = true"
+              @mouseleave="isCartHover = false"
             >
-              0
-            </span>
-            <div class="min-w-[40px]">
-              <Icon
-                name="ph:shopping-cart-simple-light"
-                size="33"
-                :color="isCartHover ? '#FF4646' : ''"
-              />
-            </div>
+              <span
+                class="absolute flex items-center justify-center -right-[3px] top-0 bg-[#FF4646] h-[17px] min-w-[17px] text-xs text-white px-0.5 rounded-full"
+              >
+                {{ userStore.cart.length }}
+              </span>
+              <div class="min-w-[40px]">
+                <Icon
+                  name="ph:shopping-cart-simple-light"
+                  size="33"
+                  :color="isCartHover ? '#FF4646' : ''"
+                />
+              </div>
+            </button>
+          </NuxtLink>
+
+          <button
+            class="md:hidden block rounded-full p-1.5 -mt-[4px] hover:bg-gray-200"
+            @click="userStore.isMenuOverlay = true"
+          >
+            <Icon name="radix-icons:hamburger-menu" size="33" />
           </button>
-        </NuxtLink>
-        <button
-          class="md:hidden block rounded-full p-1.5 -mt-[4px] hover:bg-gray-200"
-          @click="userStore.isMenuOverlay = true"
-        >
-          <Icon name="radix-icons:hamburger-menu" size="33" />
-        </button>
+        </div>
       </div>
     </div>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,91 @@
 <template>
   <MainLayout>
     <div id="IndexPage" class="mx-auto mt-4 max-w-[1200px] px-2" />
+    <div
+      v-if="products"
+      class="grid xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4"
+    >
+      <div v-for="product in products" :key="product">
+        <ProductComponent :product="product" />
+      </div>
+    </div>
   </MainLayout>
 </template>
 
 <script setup>
 import MainLayout from '~/layouts/MainLayout.vue'
+import ProductComponent from '~/components/BaseProductComponent.vue'
+
+const products = [
+  {
+    id: 1,
+    title: 'Title 1',
+    description: 'This is a description',
+    url: 'https://picsum.photos/id/70/800/800',
+    price: 9999
+  },
+  {
+    id: 1,
+    title: 'Title 1',
+    description: 'This is a description',
+    url: 'https://picsum.photos/id/71/800/800',
+    price: 9699
+  },
+  {
+    id: 1,
+    title: 'Title 1',
+    description: 'This is a description',
+    url: 'https://picsum.photos/id/72/800/800',
+    price: 9999
+  },
+  {
+    id: 1,
+    title: 'Title 1',
+    description: 'This is a description',
+    url: 'https://picsum.photos/id/73/800/800',
+    price: 29999
+  },
+  {
+    id: 1,
+    title: 'Title 1',
+    description: 'This is a description',
+    url: 'https://picsum.photos/id/74/800/800',
+    price: 9999
+  },
+  {
+    id: 1,
+    title: 'Title 1',
+    description: 'This is a description',
+    url: 'https://picsum.photos/id/75/800/800',
+    price: 9999
+  },
+  {
+    id: 1,
+    title: 'Title 1',
+    description: 'This is a description',
+    url: 'https://picsum.photos/id/76/800/800',
+    price: 9999
+  },
+  {
+    id: 1,
+    title: 'Title 1',
+    description: 'This is a description',
+    url: 'https://picsum.photos/id/77/800/800',
+    price: 9999
+  },
+  {
+    id: 1,
+    title: 'Title 1',
+    description: 'This is a description',
+    url: 'https://picsum.photos/id/78/800/800',
+    price: 9999
+  },
+  {
+    id: 1,
+    title: 'Title 1',
+    description: 'This is a description',
+    url: 'https://picsum.photos/id/79/800/800',
+    price: 6999
+  }
+]
 </script>
